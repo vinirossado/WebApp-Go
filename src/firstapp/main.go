5a891e8c1016546385fa27407d738367e54c31a7
Vinicius Rossado <vinicius_rossado@hotmail.com>
2021-09-07 19:30:00 -0300

    Teste

--- src/firstapp/main.go
+++ src/firstapp/main.go
@@ -4,7 +4,7 @@ import "net/http"
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("hello world, GO!"))
+		w.Write([]byte("hello world, GOs!"))
 	})
 	http.ListenAndServe(":8000", nil)
 }
